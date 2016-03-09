ff955e147ad58b8127fff9ff0d65ca901c654592
Shantanu Kumar <kumar.shantanu@gmail.com>
2016-03-09 16:36:56 +0530

    begin 0.3.0 development

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject cambium "0.2.0"
+(defproject cambium "0.3.0-SNAPSHOT"
   :description "Clojure wrapper for SLF4j with MDC and clojure/tools.logging"
   :url "https://github.com/kumarshantanu/cambium"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.slf4j/slf4j-api       "1.7.13"]
+  :dependencies [[org.slf4j/slf4j-api       "1.7.18"]
                  [org.clojure/tools.logging "0.3.1" :exclusions [org.clojure/clojure]]]
   :global-vars {*warn-on-reflection* true}
   :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.10"]
@@ -13,7 +13,7 @@
              :c16 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :c17 {:dependencies [[org.clojure/clojure "1.7.0"]]
                    :global-vars {*unchecked-math* :warn-on-boxed}}
-             :c18 {:dependencies [[org.clojure/clojure "1.8.0-RC3"]]
+             :c18 {:dependencies [[org.clojure/clojure "1.8.0"]]
                    :global-vars {*unchecked-math* :warn-on-boxed}}}
   :plugins [[lein-codox "0.9.4"]]
   :codox {:namespaces [cambium.core]
