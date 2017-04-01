7ae5c385cb80f4efe8d0b19d97e026c49fbd52b5
Shantanu Kumar <kumar.shantanu@gmail.com>
2017-04-01 12:18:02 +0530

    fix parent project coordinates

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject logback-bundle "not-applicable"
+(defproject cambium/cambium-parent "not-released"
   :description "Logs as data with SLF4j MDC and clojure/tools.logging"
   :url "https://github.com/kumarshantanu/cambium"
   :license {:name "Eclipse Public License"
