237d1e7aafeb14ba6f8a5bf61766e713849adc20
Shantanu Kumar <kumar.shantanu@gmail.com>
2017-04-01 12:41:54 +0530

    fix sub-module names in parent project

--- project.clj
+++ project.clj
@@ -4,5 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-sub "0.3.0"]]
-  :sub ["cambium.core"])
-
+  :sub ["cambium.codec-simple" "cambium.codec-cheshire" "cambium.core"])
